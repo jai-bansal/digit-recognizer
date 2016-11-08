d648c42ce6fb29a1152a86365420f7417a2824bb
jai-bansal <jbans79@gmail.com>
2016-11-07 16:36:32 -0800

    data inspection section complete
    completed data inspection section

--- digit_recognizer_analysis.R
+++ digit_recognizer_analysis.R
@@ -1,6 +1,7 @@
 # This script uses pixel data to predict handwritten digits.
 
 # PREP AND DATA IMPORT ----------------------------------------------------
+# This section loads libraries and data.
 
   # Load libraries.
   library(readr)
@@ -12,5 +13,35 @@
   test = data.table(read_csv('test.csv'))
 
 # DATA INSPECTION ---------------------------------------------------------
+# This section inspects the data.
+# It is commented out but results are listed.
+
+  # Check 'train' and 'test' for NA values.
+  # This takes a little while.
+  # There are no NA values.
+  table(is.na(train))
+  table(is.na(test))
+  
+  # Check 'train' and 'test' for blank values.
+  # This takes a little while.
+  # There are no blank values.
+  table(train == '')
+  table(test == '')
+  
+  # Check 'train' and 'test' for values less than 0.
+  # This takes a little while.
+  table(train < 0)
+  table(test < 0)
+  
+  # Check the minimum and maximum values of 'train' and 'test'.
+  # For both 'train' and 'test', the minimum and maximum values are 0 and 255 respectively.
+  min(train)
+  max(train)
+  min(test)
+  max(test)
+  
+
+# BASELINE MODEL ----------------------------------------------------------
+# This section implements a baseline random forest model with no feature engineering or modification.
 
   
