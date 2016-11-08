425f759653cdac5f88be54caaacf413afcc6c10d
jai-bansal <jbans79@gmail.com>
2016-11-07 17:38:24 -0800

    completed data inspection section
    completed data inspection section

--- digit_recognizer_analysis.py
+++ digit_recognizer_analysis.py
@@ -3,6 +3,7 @@
 #################
 # PREP AND IMPORT
 #################
+# This section loads modules and data.
 
 # Import modules.
 import pandas as pd
@@ -15,3 +16,34 @@ test = pd.read_csv('test.csv')
 #################
 # DATA INSPECTION
 #################
+# This section inspects the data.
+# It's commented out but results are listed.
+
+# Check out data description.
+# This isn't very helpful due to the large number of columns.
+# train.describe()
+# test.describe()
+
+# View the type of each column.
+# All columns are 'int64'.
+# train.dtypes.value_counts()
+# test.dtypes.value_counts()
+
+# Check 'train' and 'test' for null values.
+# Since all columns are 'int64' there is no need to check for blank values.
+# There are no null values.
+# train.isnull().sum().sum()
+# test.isnull().sum().sum()
+
+# Check the minimum and maximum values of 'train' and 'test'.
+# For both 'train' and 'test', the minimum and maximum values are 0 and 255 respectively.
+# So there are no negative values.
+#train.min().min()
+#train.max().max()
+#test.min().min()
+#test.max().max()
+
+################
+# BASELINE MODEL
+#################
+# This section implements a baseline random forest model with no feature engineering or modification.
