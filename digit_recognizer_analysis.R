c4bd3186c8d704e2e70de0f582545f2c915b554d
jai-bansal <jbans79@gmail.com>
2016-11-07 17:40:56 -0800

    edits to data inspection section
    edits to data inspection section

--- digit_recognizer_analysis.R
+++ digit_recognizer_analysis.R
@@ -15,32 +15,36 @@
 # DATA INSPECTION ---------------------------------------------------------
 # This section inspects the data.
 # It is commented out but results are listed.
+  
+  # View dimensions of data.
+  # dim(train)
+  # dim(test)
+  
+  # View summary of data.
+  # This isn't very helpful because of the large number of columns.
+  # summary(train)
+  # summary(test)
 
   # Check 'train' and 'test' for NA values.
   # This takes a little while.
   # There are no NA values.
-  table(is.na(train))
-  table(is.na(test))
+  # table(is.na(train))
+  # table(is.na(test))
   
   # Check 'train' and 'test' for blank values.
   # This takes a little while.
   # There are no blank values.
-  table(train == '')
-  table(test == '')
-  
-  # Check 'train' and 'test' for values less than 0.
-  # This takes a little while.
-  table(train < 0)
-  table(test < 0)
+  # table(train == '')
+  # table(test == '')
   
   # Check the minimum and maximum values of 'train' and 'test'.
   # For both 'train' and 'test', the minimum and maximum values are 0 and 255 respectively.
-  min(train)
-  max(train)
-  min(test)
-  max(test)
+  # So there are no negative values.
+  # min(train)
+  # max(train)
+  # min(test)
+  # max(test)
   
-
 # BASELINE MODEL ----------------------------------------------------------
 # This section implements a baseline random forest model with no feature engineering or modification.
 
