9008614e728a152e775dafeaecd6fd456234a7c1
jai-bansal <jbans79@gmail.com>
2016-11-08 17:09:21 -0800

    completed baseline model section
    completed baseline model section

--- digit_recognizer_analysis.R
+++ digit_recognizer_analysis.R
@@ -6,6 +6,7 @@
   # Load libraries.
   library(readr)
   library(data.table)
+  library(randomForest)
 
   # Import training and test data.
   # Working directory must be set to the 'digit_recognizer' repository folder.
@@ -45,7 +46,48 @@
   # min(test)
   # max(test)
   
+  
+
 # BASELINE MODEL ----------------------------------------------------------
 # This section implements a baseline random forest model with no feature engineering or modification.
+  
+  # Turn 'train$label' into a factor.
+  train$label = as.factor(train$label)
+  
+  # Define model.
+  baseline_model = label ~ .
+  
+  # Set seed for reproducibility.
+  set.seed(1234)
+  
+  # Create random forest.
+  # Note this takes a little while.
+  # This script is mostly instructional and takes a while to run so I use a very small number of trees.
+  baseline_rf = randomForest(baseline_model, 
+                             data = train, 
+                             ntree = 5, 
+                             importance = T)
+  
+  # Generate predictions for 'train' and 'test'.
+  train$baseline_pred = predict(baseline_rf, 
+                                newdata = train, 
+                                type = 'response')
+  test$baseline_pred = predict(baseline_rf, 
+                               newdata = test, 
+                               type = 'response')
+  
+  # Check training set accuracy.
+  table(train$label == train$baseline_pred)
+  prop.table(table(train$label == train$baseline_pred))
+  
+  # Check cross-validation accuracy.
+  # For random forest, OOB score can be used for cross-validation.
+  # OOB score is contained in the following output.
+  baseline_rf
+  
+  # Test set accuracy cannot be checked as I do not have the answers for the test set.
+
+# PCA MODEL ---------------------------------------------------------------
+# This section conducts Principal Components Analysis (PCA) before using a random forest model for prediction.
 
   
