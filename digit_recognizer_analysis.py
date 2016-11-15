4c22027fb4686653d81dfb1fe9ce89a7e491af0e
jai-bansal <jbans79@gmail.com>
2016-11-14 17:42:48 -0800

    complete PCA MODEL section
    complete PCA MODEL section. kept hitting memory errors so had to exclude
    full PCA and use subsets of data for limited PCA

--- digit_recognizer_analysis.py
+++ digit_recognizer_analysis.py
@@ -8,6 +8,8 @@
 # Import modules.
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.decomposition import PCA
 
 # Import training and test data.
 # Working directory must be set to the 'digit_recognizer' repository folder.
@@ -53,32 +55,80 @@ test = pd.read_csv('test.csv')
 #################
 # This section implements a baseline random forest model with no feature engineering or modification.
 
-# Create random forest classifier.
+# Get training set features.
+train_features = train.drop('label',
+                            axis = 1)
+
+# Create and fit random forest classifier.
 # This script is mostly instructional so I use a very small number of trees.
+# I also only use a subset of 'train' for the random forest to avoid hitting a memory error.
+# I assume the order of the data doesn't matter because I just use the first few rows (no random sampling of rows).
 baseline_rf = RandomForestClassifier(n_estimators = 25,
                                      oob_score = True,
                                      random_state = 1234)
-
-# Fit 'baseline_rf' on training data.
-baseline_rf.fit(train.drop('label',
-                           axis = 1),
+baseline_rf.fit(train_features,
                 train['label'])
 
 # Generate predictions for training and test data.
-train['baseline_pred'] = baseline_rf.predict(train.drop('label',
-                                                        axis = 1))
+train['baseline_pred'] = baseline_rf.predict(train_features)
 test['baseline_pred'] = baseline_rf.predict(test)
 
 # Compute training set accuracy.
+# Training set accuracy is 99.9976%.
 100 * sum(train['label'] == train['baseline_pred']) / len(train['baseline_pred'])
 
 # Check cross-validation accuracy.
 # For random forest, OOB score can be used for cross-validation.
-baseline_rf.oob_score_
+# OOB accuracy is 93.55%.
+print(baseline_rf.oob_score_)
 
 # Test set accuracy cannot be checked as I do not have the answers for the test set.
+
+# Delete 'baseline_rf' to avoid a memory error later.
+del(baseline_rf)
 
 ###########
 # PCA MODEL
 ###########
 # This section conducts Principal Components Analysis (PCA) before using a random forest model for prediction.
+
+# Create and fit principal component analysis objects.
+# I conduct one PCA removing many of the least useful components.
+# I exclude the full component PCA (included in the R branch) to avoid hitting a memory error.
+# I assume the order of the data doesn't matter because I just use the first few rows for PCA (no random sampling of rows).
+# Normally, I would scale 'train'. But I do not for 2 reasons:
+# 1. All of the data is pixel data ranging from 0 to 255 so the scales are identical for all variables.
+# 2. Some of the columns are all a single number (I assume 0) which makes scaling fail.
+pca = PCA(n_components = 50,
+          random_state = 1234).fit(train_features.head(3000))
+
+# Apply 'pca' to 'train' and 'test'.
+train_comp = pca.transform(train_features)
+test_comp = pca.transform(test.drop('baseline_pred',
+                                    axis = 1))
+
+# Create and fit a random forest using 'train_comp'.
+pca_rf = RandomForestClassifier(n_estimators = 25,
+                                oob_score = True,
+                                random_state = 1234)
+pca_rf.fit(train_comp,
+           train['label'])
+
+# Generate predictions for 'train' using 'pca_rf'.
+train['pca_pred'] = pca_rf.predict(train_comp)
+
+# Check training set accuracy for 'pca_rf'.
+# Training set accuracy is 99.9976%.
+# 'pca_pred' is extremely similar to 'baseline_pred'.
+# Thus, the training error for 'pca_rf' is extremely similar to the training error for 'baseline_rf'.
+100 * sum(train['label'] == train['pca_pred']) / len(train['label'])
+
+# Check cross-validation accuracy for 'pca_rf'.
+# OOB accuracy is 90.8%.
+# The OOB accuracy for 'pca_rf' is lower than the OOB accuracy for 'baseline_rf'.
+pca_rf.oob_score_
+
+# Generate predictions for 'test' using 'pca_rf'.
+test['pca_pred'] = pca_rf.predict(test_comp)
+
+# Test set accuracy cannot be checked as I do not have the answers for the test set.
